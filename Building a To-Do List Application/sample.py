e8f4c5e45a86404cb095747f16ea4ccfd8985b16
C.DIVYASRI <167788688+divyasri27k@users.noreply.github.com>
2024-06-07 22:46:31 +0530

    Add files via upload

---
+++ Building a To-Do List Application/sample.py
@@ -0,0 +1,28 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/c.divya sri/OneDrive/Documents/todo.py
+Menu:
+1. Add Task
+2. View Tasks
+3. Mark as Done
+4. Exit
+Enter your choice: 1
+Enter task description: hello
+Task added successfully!
+Menu:
+1. Add Task
+2. View Tasks
+3. Mark as Done
+4. Exit
+Enter your choice: 3
+
+Tasks:
+1. hello
+Enter task index to mark as done: 4
+Invalid task index.
+Menu:
+1. Add Task
+2. View Tasks
+3. Mark as Done
+4. Exit
